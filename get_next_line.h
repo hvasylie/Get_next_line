eafcfef27db212f37ecab85d453da163fe857cd5
Hlib Vasyliev <hvasylie@e1z2r9p18.42.us.org>
2019-06-18 20:33:34 -0700

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,15 @@
 /*   By: hvasylie <hvasylie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 14:22:46 by hvasylie          #+#    #+#             */
-/*   Updated: 2019/06/03 14:00:47 by hvasylie         ###   ########.fr       */
+/*   Updated: 2019/06/13 16:57:09 by hvasylie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-#ifndef		GET_NEXT_LINE_H
-# define	GET_NEXT_LINE_H
-# define	BUFF_SIZE 1
-# define	FD_MAX 4864
-# include	"libft/libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+# define FD_MAX 4864
+# include "libft/libft.h"
 
 int			get_next_line(const int fd, char **line);
 
